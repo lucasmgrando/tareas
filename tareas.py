190002cdbf5070ae9e0df93f94e187713f3ed64c
lucasmgrando <lucasmgrando@gmail.com>
2016-07-07 18:25:32 -0300

    Simplificar argumentos

--- tareas.py
+++ tareas.py
@@ -7,28 +7,26 @@ import sys
 # Variables globales
 cur = None # Cursor
 
-# Argumentos del programa
-parser = argparse.ArgumentParser()
-parser.add_argument('--crear-tarea', help='Crear una tarea', action='store_true')
-parser.add_argument('--descripcion', help='Agregar o modificar la descripcion de una tarea')
-
 # Operaciones
-def crear_tarea(descripcion, lista_id=1):
+def crear_tarea(descripcion):
     """ Crear una tarea con descripcion en la lista especificada,
     si no se especifica se utiliza la lista de tareas sueltas. """
 
-    cur.execute('INSERT INTO tareas (descripcion, lista_id) VALUES (?, ?)', (descripcion, lista_id))
+    cur.execute('INSERT INTO tareas (descripcion, lista_id) VALUES (?, 1)', (descripcion,))
 
 def eliminar_tarea(tarea_id):
     """ Eliminar una tarea """
+
     cur.execute('DELETE FROM tareas WHERE id=?', (tarea_id,))
 
 def actualizar_tarea(tarea_id, descripcion):
     """ Actualizar descripcion de una tarea """
+
     cur.execute('UPDATE tareas SET descripcion=? WHERE id=?', (descripcion, tarea_id,))
 
 def mostrar_tarea(tarea_id):
-    """ Mostrar la tarea segun tarea_id """
+    """ Mostrar una tarea o todas si tarea_id = 0 """
+
     if tarea_id == 0:
         cur.execute('SELECT id, descripcion FROM tareas')
     else:
@@ -37,6 +35,13 @@ def mostrar_tarea(tarea_id):
     tareas = cur.fetchall()
     for tarea in tareas:
         print "%s - %s" % (tarea[0], tarea[1],)
+
+# Argumentos del programa
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', help='Crear una tarea')
+parser.add_argument('-d', type=int, help='Eliminar tarea')
+parser.add_argument('-u', type=int, help='Actualizar tarea')
+parser.add_argument('-p', type=int, help='Mostrar tarea')
 
 def main():
 
@@ -47,8 +52,12 @@ def main():
 
     args = parser.parse_args()
 
-    if args.crear_tarea and args.descripcion:
-        crear_tarea(args.descripcion)
+    if args.a:
+        crear_tarea(args.a)
+    elif args.d:
+        eliminar_tarea(args.d)
+    elif args.p:
+        mostrar_tarea(args.p)
 
     con.commit()
     con.close()
